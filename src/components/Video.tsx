6af0d47b98dc2dddec43a789abbbf77aad51389c
RafaelRufino <rafaelrufino.52@gmail.com>
2022-06-28 18:23:49 -0300

    feat: incluindo component de video

---
+++ src/components/Video.tsx
@@ -0,0 +1,155 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
+//necessario importa esse css
+import "@vime/core/themes/default.css";
+
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    slug: string;
+    description: string;
+    videoId: string;
+    teacher: {
+      name: string;
+      avatarURL: string;
+      bio: string;
+    };
+  };
+}
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        name
+        avatarURL
+        bio
+      }
+    }
+  }
+`;
+
+export function Video({ lessonSlug }: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: { slug: lessonSlug },
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1 justify-center items-center ">
+        <p>Carregando....</p>
+      </div>
+    );
+  }
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
+        </div>
+      </div>
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              {data.lesson.description}
+            </p>
+            <div className="flex items-start gap-4 mt-6  ">
+              <img
+                className=" rounded-full h-16 w-16 border-2 border-blue-500"
+                src={data.lesson.teacher.avatarURL}
+                alt="Logo Perfil "
+              />
+              <div className="leading-relaxed">
+                <strong className=" font-bold text-2x1 block text-green-500 ">
+                  {data.lesson.teacher.name}
+                </strong>
+                <span className="text-gray-200 text-sm block">
+                  {data.lesson.teacher.bio}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-coll gap-4">
+            <a
+              target="_blank"
+              href="https://discord.com/channels/Rafael%20Rufino"
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade do Discord
+            </a>
+
+            <a
+              href=""
+              className="p-4 text-sm  flex items-center rounded font-bold uppercase gap-2 justify-center border-blue-500 border text-blue-500 hover:bg-blue-500 hover: hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href=""
+            target="_blank"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu
+                desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+
+          <a
+            href=""
+            target="_blank"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe os wallpapers exclusivos do ignite Lab e personalise a sua
+                máquina desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
